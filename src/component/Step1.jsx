2e7f60894617dac021cab69ebaa95201ea3920dd
Asifali720 <asiflashari720@gmail.com>
2023-10-07 19:43:10 +0500

    try to fix bug

--- src/component/Step1.jsx
+++ src/component/Step1.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
 import { StapperContext } from '../Contexts/StepperContext'
-import { data } from 'autoprefixer'
 
 const Step1 = () => {
   const {userData , setUserData} = useContext(StapperContext)
@@ -15,20 +14,20 @@ const Step1 = () => {
       <h2 className='text-center font-bold text-3xl text-green-600'>Your Details</h2>
       <p className='text-base leading-6 text-center text-gray-600 mb-6'>please put about your information!</p>
       <p className='text-base leading-6 text-black mb-2'>Name</p>
-      <input type="text" name="" id="" value={userData['text'] || ''} onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600 mb-6' placeholder='username' required/>
+      <input type="text" name="" id="" onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600 mb-6' placeholder='username' required/>
       <div className='w-full flex flex-col lg:flex-row items-start lg:items-center gap-6'>
         <div>
         <p className='text-base leading-6 text-black mb-2'>Email</p>
-      <input type="email" name="" id="" onChange={handleChange} value={userData['email'] || ''} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='email' required/>
+      <input type="email" name="" id="" onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='email' required/>
         </div>
         <div>
         <p className='text-base leading-6 text-black mb-2'>Resume</p>
-      <input type="file" name="" id="" onChange={handleChange} value={userData['file'] || ''} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='email' required/>
+      <input type="file" name="" id="" onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='email' required/>
         </div>
         <div>
         <p className='text-base leading-6 text-black mb-2'>Phone Number</p>
-      <input type="tel" name="" id="" onChange={handleChange} value={userData['tel'] || ''} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='number' required/>
-        </div> 
+      <input type="tel" name="" id="" onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600' placeholder='number' required/>
+        </div>
       </div>
       <p className='text-base leading-6 text-black mb-2'>Address</p>
       <input type="text" name="" id="" onChange={handleChange} className='w-full rounded-md p-4 placeholder:text-gray-400 text-green-600 outline-none focus:outline-green-600 mb-6' placeholder='Address' required/>
